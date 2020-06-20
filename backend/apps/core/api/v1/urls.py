ee908c2d288f78b8ba61e4f7b91eb3d177e53f5f
kiselev Vladimir <wsxxx2016@yandex.ru>
2020-06-20 04:38:51 +0300

    Plugged urls applications in core url file

--- backend/apps/core/api/v1/urls.py
+++ backend/apps/core/api/v1/urls.py
@@ -4,4 +4,6 @@ app_name = "v1"
 
 urlpatterns = [
     path("auth/", include("apps.api.auth.v1")),
+    path("offers/", include("apps.api.offers.v1")),
+    path("voting/", include("apps.api.voting.v1")),
 ]
